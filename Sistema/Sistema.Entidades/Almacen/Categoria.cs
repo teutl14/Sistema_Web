52b2aee13813cd0a493a08ea866a89d042c5ef60
Teutla14 <40640139+Teutla14@users.noreply.github.com>
2019-01-07 17:54:29 -0600

    GetSet_TablaCategoria

---
+++ Sistema/Sistema.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Entidades.Almacen
+{
+    public class Categoria
+    {
+        public int idcategoria { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+    }
+}
