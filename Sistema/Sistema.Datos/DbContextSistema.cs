d3aa8eeba76db782af44ddc1cdb1ab2a8b0f9fb6
Teutla14 <40640139+Teutla14@users.noreply.github.com>
2019-01-09 17:58:20 -0600

    Feature_Over

--- Sistema/Sistema.Datos/DbContextSistema.cs
+++ Sistema/Sistema.Datos/DbContextSistema.cs
@@ -7,5 +7,13 @@ namespace Sistema.Datos
 {
    public class DbContextSistema : DbContext
     {
+        public DbContextSistema(DbContextOptions<DbContextSistema> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
