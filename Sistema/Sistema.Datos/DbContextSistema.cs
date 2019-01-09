0d33c91410cb2c922cce6fe21fa0c8f0d5b99c44
Teutla14 <40640139+Teutla14@users.noreply.github.com>
2019-01-09 17:29:37 -0600

    AddEntityFramework

---
+++ Sistema/Sistema.Datos/DbContextSistema.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Datos
+{
+   public class DbContextSistema : DbContext
+    {
+    }
+}
