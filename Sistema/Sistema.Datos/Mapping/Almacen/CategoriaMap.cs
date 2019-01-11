353dc259b6009f2e0b665adaefb511719542429b
Teutla14 <40640139+Teutla14@users.noreply.github.com>
2019-01-11 16:46:26 -0600

    FeauteCambio2

---
+++ Sistema/Sistema.Datos/Mapping/Almacen/CategoriaMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Datos.Mapping.Almacen
+{ 
+    class CategoriaMap : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("categoria")
+                .HasKey(c => c.idcategoria);
+        }
+    }
+}
